10e44054b3a5349f4d7d8dbd3e28a72b4f6dfd54
suthyscott <sutherlandscott21@gmail.com>
2023-03-01 15:57:27 -0700

    some detail page styling

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,33 +1,43 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import axios from "axios"
+import "./Details.css"
 
 const Details = () => {
-  const {destinationId} = useParams()
-  const [destination, setDestination] = useState({})
+    const { destinationId } = useParams()
+    const [destination, setDestination] = useState({})
 
-  const getDestination = () => {
-    axios.get(`http://localhost:4545/api/destination/${destinationId}`)
-      .then(res => {
-        console.log(res.data)
-        setDestination(res.data)
-      })
-      .catch(err => console.log(err))
-  }
+    const getDestination = () => {
+        axios
+            .get(`http://localhost:4545/api/destination/${destinationId}`)
+            .then(res => {
+                console.log(res.data)
+                setDestination(res.data)
+            })
+            .catch(err => console.log(err))
+    }
 
-  useEffect(() => {
-    getDestination()
-  }, [])
- 
-  console.log(destination)
-  return (
-    <div>
-      <p>{destination.name}</p>
-      <p>International: {destination.international ? 'True': 'False'}</p>
-      <img src={destination.imageURL}/>
-      <p>{destination.notes}</p>
-    </div>
-  )
+    useEffect(() => {
+        getDestination()
+    }, [])
+
+    console.log(destination)
+    return (
+        <div className="detail-container">
+            <div id="detail-banner" style={{
+              backgroundImage: `url(${destination.imageURL})`,
+              backgroundSize: "cover",
+            }}>
+                <p>{destination.name}</p>
+            </div>
+            <section id="detail-info-box">
+                <p>
+                    International:{destination.international ? "True" : "False"}
+                </p>
+                <p>{destination.notes}</p>
+            </section>
+        </div>
+    )
 }
 
 export default Details
